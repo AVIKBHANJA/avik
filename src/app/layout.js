8d19418dff265923adf02ab4a123c7b97ed06965
AVIKBHANJA <avikbhanja3@gmail.com>
2025-01-20 10:17:50 +0530

    added vercel speed insight

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,15 +1,16 @@
-import { Inter } from 'next/font/google';
-import './globals.css';
-import { ThemeProvider } from '@/context/ThemeContext';
-import FooterNavigation from '@/components/FooterNavigation';
-import Navbar from '@/components/Navbar';
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Inter } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/context/ThemeContext";
+import FooterNavigation from "@/components/FooterNavigation";
+import Navbar from "@/components/Navbar";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const inter = Inter({ subsets: ['latin'] });
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Professional Portfolio',
-  description: 'Avik Bhanja , a Full-stack developer portfolio showcasing projects and skills',
+  title: "Professional Portfolio",
+  description:
+    "Avik Bhanja , a Full-stack developer portfolio showcasing projects and skills",
 };
 
 export default function RootLayout({ children }) {
@@ -18,8 +19,8 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
         <ThemeProvider>
           <Navbar />
-          <SpeedInsights />
           {children}
+          <SpeedInsights />
           <FooterNavigation />
         </ThemeProvider>
       </body>
